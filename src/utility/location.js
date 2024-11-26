c126d0fd153783310342bd058f002c1f8f24e142
Kuldeep-Sonara <kuldeep.sonara@prominentpixel.com>
2024-11-26 16:36:44 +0530

    updtae

--- src/utility/location.js
+++ src/utility/location.js
@@ -1,26 +1,23 @@
 const shareLinkInput = document.getElementById('share-link');
 const shareBtn = document.getElementById('share-btn');
 
-// Function to update the map with a new location and generate the shareable link
 const updateMapAndCreateLink = (latitude, longitude, textContent) => {
-  // Update the map view to the new coordinates
   map.setView([latitude, longitude], 13);
 
-  // Add a marker and popup at the new location
   L.marker([latitude, longitude])
     .addTo(map)
-    .bindPopup(`<b>${textContent}</b>`) // Show a popup at the user's location
+    .bindPopup(`<b>${textContent}</b>`) 
     .openPopup();
 
-  // Create the shareable link
+
   // const shareableLink = `https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=13/${latitude}/${longitude}`;
   
-  const currentUrl = window.location.origin; // Get the base URL (e.g., http://localhost:3000)
-  const shareableLink = `${currentUrl}/share%20plase/dist/vister.html?lat=${latitude}&lon=${longitude}`;
+  const currentUrl = window.location.origin; 
+  const shareableLink = `${currentUrl}/dist/vister.html?lat=${latitude}&lon=${longitude}`;
 
-  // Display the link in the input field and enable the Share button
+  
   shareLinkInput.value = shareableLink;
-  shareBtn.disabled = false; // Enable the Share button
+  shareBtn.disabled = false; 
 
   console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 };
